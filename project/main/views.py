9ef8ced4477c65e0702aba34b14600a756ced9be
kochnev <lyotchik1@mail.ru>
2017-02-01 17:58:15 +0300

    add login

--- project/main/views.py
+++ project/main/views.py
@@ -1,12 +1,35 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth import authenticate, login
+
+from main.forms import LoginForm
 # Create your views here.
 
 def index(request):
     return render(request, 'main/index.html', {})
 
+#def signin(request):
+#    return render(request, 'main/signin.html', {})
+
 def signin(request):
-    return render(request, 'main/signin.html', {})
+    message = ''
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return HttpResponseRedirect('/')
+            else:
+                message = 'username or password are incorrect'
+    else:
+        form = LoginForm()
+    return render(request, 'main/signin.html',{
+        'form': form,
+        'message':message,
+})
 
 def signup(request):
         return render(request, 'main/signup.html', {})
