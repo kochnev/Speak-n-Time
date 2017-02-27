52782e515338f06d46f1b516ff1abb35dfc015b9
Alexander <lyotchik1@mail.ru>
2017-02-27 17:23:22 +0300

    add bootsrap

--- project/project/urls.py
+++ project/project/urls.py
@@ -15,10 +15,18 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from registration.backends.simple.views import RegistrationView
+
+# Create a new class that redirects the user to the index page,
+# if successful at logging
+class MyRegistrationView(RegistrationView):
+    def get_success_url(self, user):
+        return '/'
 
 
 urlpatterns = [
     url(r'^', include('main.urls')),
+    url(r'^accounts/register/$', MyRegistrationView.as_view(), name='registration_register'),
     url(r'^accounts/', include('registration.backends.simple.urls')),
     url(r'^admin/', admin.site.urls),
 ]
