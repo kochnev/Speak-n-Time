272969c62001952102ba84b6e90f5a96ad494622
Alexander <lyotchik1@mail.ru>
2017-02-28 21:09:42 +0300

    add profile page

--- project/main/urls.py
+++ project/main/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^register_profile/$', views.register_profile, name='register_profile'),
+    url(r'^profile/(?P<username>[\w\-]+)/$', views.profile, name='profile'),
 ]
--- project/main/views.py
+++ project/main/views.py
@@ -1,12 +1,34 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from main.forms import UserProfileForm
+from main.models import UserProfile
 # Create your views here.
 
 def index(request):
     return render(request, 'main/index.html', {})
+
+@login_required
+def profile(request, username):
+    try:
+        user = User.objects.get(username=username)
+    except User.DoesNotExist:
+        return redirect('index')
+    userprofile = UserProfile.objects.get_or_create(user=user)[0]
+    form = UserProfileForm(
+    {'website': userprofile.website, 'picture': userprofile.picture})
+
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, request.FILES, instance=userprofile)
+        if form.is_valid():
+            form.save(commit=True)
+            return redirect('profile', user.username)
+        else:
+            print(form.errors)
+    return render(request, 'main/profile.html',
+    {'userprofile': userprofile, 'selecteduser': user, 'form': form})
 
 @login_required
 def register_profile(request):
@@ -24,4 +46,4 @@ def register_profile(request):
 
     context_dict = {'form':form}
 
-    return render(request, 'registration/registration_profile.html', context_dict)
+    return render(request, 'main/registration_profile.html', context_dict)
