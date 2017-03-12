530c53d23ce7e3d4411d58f67f010c857190facb
Alexander <lyotchik1@mail.ru>
2017-03-12 21:04:25 +0300

    add language to user profile

--- project/main/admin.py
+++ project/main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import UserProfile, Language, UserLanguage
 
 # Register your models here.
+admin.site.register(UserProfile)
+admin.site.register(Language)
+admin.site.register(UserLanguage) 
--- project/main/models.py
+++ project/main/models.py
@@ -1,8 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Language(models.Model):
+    """Model reprsenting a language (e.g Engilsh, Russian)"""
+
+    name = models.CharField(max_length=50)
+    code = models.CharField(max_length=2)
+
+    def __str__(self):
+        """ String for representing Language object """
+        return self.name
+
+
 # Create your models here.
 class UserProfile(models.Model):
+    """Model representing a userprofile (additional info about user) """
     # This line is required. Links UserProfile to a User model instance.
     user = models.OneToOneField(User)
 
@@ -11,6 +23,42 @@ class UserProfile(models.Model):
     website = models.URLField(blank=True)
     picture = models.ImageField(upload_to='profile_images', blank=True)
 
+    languages = models.ManyToManyField(
+        Language,
+        through='UserLanguage',
+        related_name='native_language',
+        help_text='Input language(e.g. english, russian, ukranian)')
+
     # Override the __inicode__() method to return out something meaningful!
-    def __unicode__(self):
+
+    GENDER = (
+        ('m', 'male'),
+        ('f', 'female'),
+    )
+
+    gender = models.CharField(max_length=1, default='m', choices=GENDER, help_text='Choose your gender')
+
+    def __str__(self):
+        """String for representing the Model object"""
         return self.user.username
+
+class UserLanguage(models.Model):
+    """Model representing a ling between user and language"""
+    user_profile = models.ForeignKey(UserProfile)
+    language = models.ForeignKey(Language)
+    native = models.BooleanField(default=False)
+
+    LEVEL = (
+        ('A1', 'Beginner'),
+        ('A2', 'Elementary'),
+        ('B1', 'Intermediate'),
+        ('B2', 'Upper intermediate'),
+        ('C1', 'Advanced'),
+        ('C2', 'Proficient'),
+    )
+
+    level = models.CharField(max_length=2, choices=LEVEL, help_text='Choose your level', blank=True)
+
+    def __str__(self):
+        """String for representing the Model object"""
+        return self.user_profile.user.username + ' ' + self.language.name
