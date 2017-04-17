d8977da431ca2c6063849911940ec376fd5de292
Alexander <lyotchik1@mail.ru>
2017-04-17 17:44:09 +0300

    set up gmail

--- etc/gunicorn/gunicorn.conf.py
+++ etc/gunicorn/gunicorn.conf.py
@@ -1,6 +1,6 @@
 bind = "127.0.0.1:9000"                   # Don't use port 80 becaue nginx occupied it already.
-errorlog = '/Users/alex/Web/readyToSpeak/etc/gunicorn/logs/gunicorn-error.log'  # Make sure you have the log folder create
-accesslog = '/Users/alex/Web/readyToSpeak/etc/gunicorn/logs/gunicorn-access.log'
-loglevel = 'debug'
+errorlog = '/Users/alex/Web/Speak-n-Time/etc/gunicorn/logs/gunicorn-error.log'  # Make sure you have the log folder create
+accesslog = '/Users/alex/Web/Speak-n-Time/etc/gunicorn/logs/gunicorn-access.log'
+loglevel = 'error'
 workers = 3   # the number of recommended workers is '2 * number of CPUs + 1'
 reload = True
--- project/project/settings.py
+++ project/project/settings.py
@@ -155,5 +155,11 @@ STATIC_ROOT = os.path.join(BASE_BASE_DIR, 'static/')
 MEDIA_ROOT = os.path.join(BASE_BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
 
-EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
-EMAIL_FILE_PATH='/Users/alex/Web/readyToSpeak/etc/tmp/django-email-dev'
+#EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+#EMAIL_FILE_PATH='/Users/alex/Web/readyToSpeak/etc/tmp/django-email-dev'
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_PORT=587
+EMAIL_HOST_USER = EMAIL_USER
+EMAIL_HOST_PASSWORD = EMAIL_PASSWORD
+EMAIL_USE_TLS=True
